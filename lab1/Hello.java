d46f83da0776d2dea2d6812ca33fa5440d6f62fd
korapat699-7 <korapatseedamart.s@kkumail.com>
2023-11-17 10:36:52 +0700

    add lab1

---
+++ lab1/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static void main(String[] args) {
+        boolean bool = true;
+        if (bool == true) {
+            for (int i = 1; i <= 1; i++) {
+                System.out.println("Hello World");
+            }
+        } else if (bool == false) {
+            System.out.println("Sayonara World");
+        }
+    }
+}
